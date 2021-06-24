1939684119b0f4de86deb722eab4636f08c13d2f
p-adams <ghusiyy@gmail.com>
2021-06-23 21:44:34 -0700

    create basic stepper

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>Home</div>
-  <router-view />
+  <div>
+    Home
+    <ul>
+      <li v-for="(step, key) in 4" :key="key">
+        <span>{{ step }}</span>
+      </li>
+    </ul>
+    <router-view />
+  </div>
 </template>
 
 <script setup>
 import { defineProps, reactive } from "vue";
-
 defineProps({
   msg: String,
 });
@@ -13,4 +19,21 @@ defineProps({
 const state = reactive({ count: 0 });
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  outline: 1px solid lightgray;
+  border-radius: 2px;
+  height: 350px;
+  display: grid;
+}
+ul {
+  align-self: end;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+li {
+  padding: 10px;
+  display: inline-block;
+}
+</style>
