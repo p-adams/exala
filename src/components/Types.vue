0f6697ae1c3c29d2691694e1041665c003364551
p-adams <ghusiyy@gmail.com>
2021-06-28 10:15:07 -0700

    add type columns

--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,7 +1,28 @@
-<template><i>najasa</i> types</template>
+<template>
+  <div class="types-container">
+    <div class="type">
+      <h4>light</h4>
+    </div>
+    <div class="type">
+      <h4>medium</h4>
+    </div>
+    <div class="type">
+      <h4>heavy</h4>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.types-container {
+  grid-template-columns: auto auto auto;
+}
+.type {
+  color: slategray;
+  border: 1px solid lightgray;
+  border-radius: 2px;
+}
+</style>
